909038bc6eae209400a9897239e6262187053fdc
AminNasirov <amin_nasirov@yahoo.com>
2022-02-08 12:41:14 -0500

    Second Commit 2

--- src/test/java/com/cydeo/tests/day3_cssSelector_Xpath/TC1_NextBaseCRM.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_Xpath/TC1_NextBaseCRM.java
@@ -1,16 +1,17 @@
 package com.cydeo.tests.day3_cssSelector_Xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC1_NextBaseCRM {
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+      //  WebDriverManager.chromedriver().setup();
+      //  WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         driver.manage().window().maximize();
 
         driver.get("https://login1.nextbasecrm.com/");
