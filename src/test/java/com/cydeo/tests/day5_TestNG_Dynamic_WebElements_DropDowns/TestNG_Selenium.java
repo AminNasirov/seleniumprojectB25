8adac1bb5beda078dff84b4a7ed097e2dd6ee2ee
AminNasirov <amin_nasirov@yahoo.com>
2022-02-15 16:08:05 -0500

    2.15.2022 (3)

---
+++ src/test/java/com/cydeo/tests/day5_TestNG_Dynamic_WebElements_DropDowns/TestNG_Selenium.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day5_TestNG_Dynamic_WebElements_DropDowns;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+         driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void selenium_test(){
+
+        driver.get("https://google.com");
+
+        Assert.assertEquals(driver.getTitle(),"Google","Title is not matching here");
+    }
+
+}
