21d9c6e3cebdc12b016dd0511898722eb1a40e0a
AminNasirov <amin_nasirov@yahoo.com>
2022-02-19 15:05:43 -0500

    2.1.2022 (1)

---
+++ src/test/java/com/cydeo/tests/day_6_alerts_iframes_windows/NextBaseCRM_login.java
@@ -0,0 +1,178 @@
+package com.cydeo.tests.day_6_alerts_iframes_windows;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class NextBaseCRM_login {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com/");
+    }
+
+
+
+
+    @Test
+    public void test1_TittleVerify(){
+
+        String expectedTitle = "Authorization";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
+    @Test
+    public void test2_CheckboxLabelVerify(){
+
+        WebElement checkboxLabel = driver.findElement(By.xpath("//label[.='Remember me on this computer']"));
+
+        String expectedCheckboxLabel = "Remember me on this computer";
+        String actualCheckboxLabel = checkboxLabel.getText();
+
+        Assert.assertEquals(actualCheckboxLabel,expectedCheckboxLabel);
+    }
+
+    @Test
+    public void test3_HR_user_login(){
+
+        driver.getWindowHandle();
+
+        WebElement userLogin = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userLogin.sendKeys("hr70@cydeo.com");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        userPassword.sendKeys("UserUser");
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
+        WebElement userName = driver.findElement(By.xpath("//span[.='Hr70 Cydeo']"));
+        Assert.assertTrue(userName.isEnabled());
+
+
+    }
+
+    @Test
+    public void test4_Marketing_user_login(){
+
+        WebElement userLogin = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userLogin.sendKeys("marketing70@cydeo.com");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        userPassword.sendKeys("UserUser");
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
+        WebElement userName = driver.findElement(By.xpath("//span[.='Marketing70 Cydeo']"));
+        Assert.assertTrue(userName.isEnabled());
+
+
+    }
+
+    @Test
+    public void test5_HelpDesk_user_login(){
+
+        WebElement userLogin = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userLogin.sendKeys("helpdesk70@cydeo.com");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        userPassword.sendKeys("UserUser");
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
+
+        WebElement userName = driver.findElement(By.xpath("//span[.='Helpdesk70 Cydeo']"));
+        Assert.assertTrue(userName.isEnabled());
+
+
+    }
+
+    @Test
+    public void test6_Invalid_user_login()  {
+
+        WebElement userLogin = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userLogin.sendKeys("Amin70@cydeo.com");
+        WebElement userPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        userPassword.sendKeys("Amin");
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+        WebElement errorText = driver.findElement(By.xpath("//div[.='Incorrect login or password']"));
+        System.out.println("errorText is displayed with incorrect login = " + errorText.isDisplayed());
+
+
+        String actualErrorText = errorText.getText();
+        String expectedErrorText = "Incorrect username or password";
+
+        Assert.assertEquals(actualErrorText,expectedErrorText);
+
+
+
+    }
+
+
+
+
+
+
+}
+/*
+TestCase1-Title Verification
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3.verify the title
+expected title "Authorization"
+
+TestCase2 - Checkbox label verification
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3.verify checkbox label
+expected checkbox label  is  “Remember me on this computer”
+
+TestCase3 - Verify Users as HR
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3. As HR user log in
+enter login:"hr70@cydeo.com" password: UserUser
+4. verify log in successfully
+
+TestCase4 - Verify Users as Marketing
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3. As Marketing user log in
+enter login: marketing70@cydeo.com  password: UserUser
+4. verify login successfully
+
+TestCase5 - Verify Users as HelpDesk
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3. As HelpDesk user log in
+enter login: helpdesk70@cydeo.com password: UserUser
+4. verify login successfully
+
+TestCase6 - Verify invalid login attempt
+1.Open Chrome Browser
+2.log in https://login2.nextbasecrm.com/
+3. enter Incorrect login  and  password
+enter login:    Amin70@cydeo.com
+      password: Amin
+4. verify  error Text
+ expectedErrorText "Incorrect username or password"
+
+
+
+
+
+ */
---
+++ src/test/java/com/cydeo/tests/day_6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,78 @@
+package com.cydeo.tests.day_6_alerts_iframes_windows;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/windows");
+    }
+
+
+
+    @Test
+    public void multiple_windows_test(){
+
+        String mainHandle = driver.getWindowHandle();
+        System.out.println("mainHandle = " + mainHandle);
+
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+        System.out.println("Title before click: "+actualTitle);
+
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+
+        clickHereLink.click();
+
+        //Set<String> allWindowHandles = driver.getWindowHandles();
+
+
+        for (String each : driver.getWindowHandles()) {
+            driver.switchTo().window(each);
+            System.out.println("current title while switching windows: "+driver.getTitle());
+        }
+
+        String expectedTitleAfterClick = "New Window";
+        actualTitle=driver.getTitle();
+
+
+
+        Assert.assertEquals(actualTitle,expectedTitleAfterClick);
+
+
+        System.out.println("Title after click: " +actualTitle);
+
+        //If we want to go back to main page, we can use already stored main handle
+        //driver.switchTo().window(mainHandle);
+
+
+
+    }
+}
+/*
+TC #5: Window Handle practice
+
+1. Create a new class called: T5_WindowsPractice
+2. Create new test and make set ups
+3. Go to : https://practice.cydeo.com/windows
+4. Assert: Title is “Windows”
+5. Click to: “Click Here” link
+6. Switch to new Window.
+7. Assert: Title is “New Window”
+ */
