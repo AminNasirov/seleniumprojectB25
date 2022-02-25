2938cbb239226b76ad59b6ffeaa33c1b70ec0a72
AminNasirov <amin_nasirov@yahoo.com>
2022-02-25 16:08:23 -0500

    Properties

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/ReadingProperties.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void test_reading_from_properties() throws IOException {
+        Properties properties = new Properties();
+
+        FileInputStream file = new FileInputStream("configuration.properties");
+
+        properties.load(file);
+
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"env\") = " + properties.getProperty("env"));
+        System.out.println("properties.getProperty(\"username\") = " + properties.getProperty("username"));
+
+    }
+}
--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,10 +1,12 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.tests.utilities.WebDriverFactory;
+import com.cydeo.tests.utilities.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,6 +24,11 @@ public class T1_WebTable_Order_Verify {
         driver.get("https://practice.cydeo.com/web-tables");
     }
 
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
     @Test
     public void test_order_name_verify() {
 
@@ -34,12 +41,24 @@ public class T1_WebTable_Order_Verify {
 
         WebElement bobMartinOrderDate =
                 driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
-
+  //        //td[.='Bob Martin']/../td[5]
 
         String expectedBobDate = "12/31/2021";
         String actualBobDate = bobMartinOrderDate.getText();
 
         Assert.assertEquals(actualBobDate,expectedBobDate);
+    }
+
+    @Test
+    public void test2() {
+       String customerOrderDate1 = WebTableUtils.returnOrderDate(driver,"Alexandra Gray");
+        System.out.println("customerOrderDate1 = " + customerOrderDate1);
+    }
+
+    @Test
+    public void test3() {
+
+        WebTableUtils.orderVerify(driver,"Bob Martin","12/31/2021");
     }
 }
 /*
